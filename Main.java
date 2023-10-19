83169821df59366021d9493e981d770ee877b372
Viteque <230790v@gmail.com>
2023-10-19 23:23:08 +0600

    New

---
+++ Main.java
@@ -0,0 +1,100 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String flowersName = "Roses";
+        System.out.println(flowersName);
+
+        int flowersNumber = 101;
+        System.out.println(flowersNumber);
+
+        int a = 15;
+        int b = 10;
+        System.out.println(a + b);
+
+        int c = a + b;
+        System.out.println(c);
+
+//            int a = 4;
+//            byte b = (byte)a;
+//            byte c = 7;
+//            int d = c;
+//            System.out.println(d);
+            /*
+            byte -> short -> int -> long
+            int -> double
+            short -> float -> double
+            char -> int
+             */
+
+//            int number = 321546513;
+//            float f1 = number;
+//            System.out.println(f1);
+//
+//            long longA = 4;
+//            int b = (int)longA;
+//            System.out.println(b);
+//
+//            int a = 258;
+//            byte c = (byte)a;
+//            System.out.println(c);
+
+//            double a = 56.9867;
+//            int b = (int)Math.round(a);
+//            System.out.println(b);
+
+            /*
+            int -> float
+            long -> float
+            long -> double
+             */
+
+    }
+
+}
+         /*
+            int x = 10;
+
+            int firstnumer;
+            firstnumer = 100;
+
+            System.out.println(firstnumer);
+            firstnumer = 200;
+            System.out.println(firstnumer);
+
+            byte a = 10, b =- 20;
+            short s = 1000, r = -5000;
+            long c = 10000L, d = -10000L;
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(s);
+            System.out.println(r);
+            System.out.println(c);
+            System.out.println(d);
+
+            double d1 = 2.6;
+            float f1 = 35e3f;
+            System.out.println(d1);
+            System.out.println(f1);
+
+            boolean one = true;
+            boolean two = false;
+
+            System.out.println(one);
+            System.out.println(two);
+
+            char letterA = 'A';
+            System.out.println(letterA);
+
+            char letterB = 66, letterC = 67;
+
+            System.out.println(letterB);
+            System.out.println(letterC);
+
+            String name = "Maria";
+            System.out.println(name);
+        }
+}
+
+          */
+
