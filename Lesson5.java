070048ffaad0b2594edbcf94bc48f23060a578bd
Viteque <23.07.90v@gmail.com>
2023-10-30 23:58:41 +0600

    Lesson5

---
+++ Lesson5.java
@@ -0,0 +1,13 @@
+public class Lesson5 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 40;
+
+        if (a == b){
+            System.out.println("Квадрат");
+        }else {
+            System.out.println("Прямоугольник");
+        }
+
+    }
+}
---
+++ qbe5.java
@@ -0,0 +1,66 @@
+public class qbe5 {
+    public static void main(String[] args) {
+//        int x = 10;
+//        int y = 5;
+//        if (10 > 5) {
+//            System.out.println("x больше y");
+//        }
+//        int time = 22;
+//        if (time < 18){
+//            System.out.println("Добрый день!");
+//
+//        }else {
+//            System.out.println("Добрый вечер!");
+//        }
+//        if (time<12){
+//           System.out.println("Доброе утро");
+//        }else if (time <18) {
+//            System.out.println("Добрый день");
+//        }else {
+//            System.out.println("Добрый вечер");
+//        }
+//        String result = (time < 18)? "ДОбрый день" : "Добрый вечер";
+//        System.out.print(result);
+//        int day = 1;
+//        switch (day) {
+//            case 6: {
+//                System.out.println("Сегодня суббота");
+//                break;
+//            }
+//            case 7: {
+//                System.out.println("Сегодня воскресенье");
+//                break;
+//                }
+//            default:
+//                System.out.println("Ждем выходных");
+//            }
+//        }
+//            case 1: {
+//                System.out.println("Понедельник");
+//                break;
+//            }
+//            case 2: {
+//                System.out.println("Вторник");
+//                break;
+//            }
+//            case 3: {
+//                System.out.println("Среда");
+//                break;
+//            }
+//            case 4: {
+//                System.out.println("Четверг");
+//                break;
+//            }
+//            case 5: {
+//                System.out.println("Пятница");
+//                break;
+//            }
+//            case 6: {
+//                System.out.println("Суббота");
+//                break;
+//            }
+//            case 7: {
+//                System.out.println("Воскресенье");
+//                break;
+//            }
+//        }
