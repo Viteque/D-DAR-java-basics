2cbf821c8a1acc4f1aa395f48fef7134f9a93b61
Viteque <23.07.90v@gmail.com>
2023-11-02 19:04:23 +0600

    Update

--- Lesson5.java
+++ Lesson5.java
@@ -1,7 +1,7 @@
 public class Lesson5 {
     public static void main(String[] args) {
-        int a = 20;
-        int b = 40;
+        int a = 40;
+        int b = 20;
 
         if (a == b){
             System.out.println("Квадрат");
@@ -9,5 +9,32 @@ public class Lesson5 {
             System.out.println("Прямоугольник");
         }
 
+//        int a = 6000;
+//        if (a > 5000) {
+//            System.out.println(a * 0.1 - a);
+//        } else {
+//            System.out.println(a);
+//        }
+
+//        int grade = 100;
+//        char letterGrade = 0;
+//        if (grade >= 0 && grade <= 25) {
+//            letterGrade = 'F';
+//        } else if (grade >= 25 && grade <= 45) {
+//            letterGrade = 'E';
+//        } else if (grade >= 45 && grade <= 50) {
+//            letterGrade = 'D';
+//        } else if (grade >= 50 && grade <= 60) {
+//            letterGrade = 'C';
+//        } else if (grade >= 60 && grade <= 80) {
+//            letterGrade = 'B';
+//        } else if (grade >= 80 && grade <= 100) {
+//            letterGrade = 'A';
+//        }
+//        System.out.println("Grade: " + grade + ", Letter: " + letterGrade);
+//
+//
+//
+//
     }
 }
--- qbe5.java
+++ qbe5.java
@@ -1,10 +1,12 @@
-public class qbe5 {
-    public static void main(String[] args) {
+//public class qbe5 {
+//    public static void main(String[] args) {
 //        int x = 10;
 //        int y = 5;
 //        if (10 > 5) {
 //            System.out.println("x больше y");
 //        }
+//    }
+//}
 //        int time = 22;
 //        if (time < 18){
 //            System.out.println("Добрый день!");
@@ -62,5 +64,62 @@ public class qbe5 {
 //            case 7: {
 //                System.out.println("Воскресенье");
 //                break;
-//            }
+public class qbe5 {
+    public static void main(String[] args) {
+//        int i = 0;
+//        while (i < 5) {   //While
+//            System.out.println(i);
+//            i++;
 //        }
+//        int i = 0;
+//        do {  //Do-while
+//            System.out.println(i);
+//            i++;
+//
+//        }
+//        while (i < 5);
+//        for (int i = 0; i < 5; i++) {  //For
+//            System.out.println(i);
+//        }
+//        for (int i = 0; i <= 10; i += 2)
+//            System.out.println(i);
+//        String[] fruits = {"Apple", "Banana", "Avocado"};
+//        for (String fruit : fruits) {
+//            System.out.println(fruit);
+//        }
+//
+//        for (int i = 0; i < 6; i++) {
+//            if (i == 3) {
+//                break;  //BREAK
+//            }
+//            System.out.println(i);
+//        }
+
+//        for (int i = 0; i < 6; i++) {
+//            if (i == 3) {
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+//        int i = 0;
+//        while (i < 6) {
+//            if (i == 3)
+//                break;
+//            System.out.println(i);
+//            i++;
+//        }
+//        int i = 0;
+//        while (i < 6) {
+//            if (i == 3) {
+//                i++;
+//                continue;
+//            }
+//            System.out.println(i);
+//            i++;
+//        }
+
+    }
+}
+
+
+
